ba85781d79d4fccb53fc7ad49c65c15d3aee8e1e
Jeff Davis <jeff@j-davis.com>
2022-11-09 08:58:08 -0800

    Introduce pg_strcoll() and pg_strncoll().
    Hide the special cases of the platform, collation provider, and
    database encoding in pg_locale.c. Simplify varlena.c.

--- src/backend/utils/adt/pg_locale.c
+++ src/backend/utils/adt/pg_locale.c
@@ -79,6 +79,12 @@
 #include <shlwapi.h>
 #endif
 
+/*
+ * This should be large enough that most strings will fit, but small enough
+ * that we feel comfortable putting it on the stack
+ */
+#define		TEXTBUFLEN			1024
+
 #define		MAX_L10N_DATA		80
 
 
@@ -1731,6 +1737,230 @@ get_collation_actual_version(char collprovider, const char *collcollate)
 	return collversion;
 }
 
+/*
+ * pg_strcoll
+ *
+ * Call ucol_strcollUTF8(), ucol_strcoll(), strcoll(), strcoll_l(), wcscoll(),
+ * or wcscoll_l() as appropriate for the given locale, platform, and database
+ * encoding. If the locale is not specified, use the database collation.
+ *
+ * Arguments must be encoded in the database encoding and nul-terminated.
+ *
+ * If the collation is deterministic, break ties with strcmp().
+ */
+int
+pg_strcoll(const char *arg1, const char *arg2, pg_locale_t locale)
+{
+	char		a1buf[TEXTBUFLEN];
+	char		a2buf[TEXTBUFLEN];
+	size_t		len1 = strlen(arg1);
+	size_t		len2 = strlen(arg2);
+	char	   *a1p,
+			   *a2p;
+	int			result;
+
+#ifdef WIN32
+	/* Win32 does not have UTF-8, so we need to map to UTF-16 */
+	if (GetDatabaseEncoding() == PG_UTF8
+		&& (!locale || locale->provider == COLLPROVIDER_LIBC))
+	{
+		int			a1len;
+		int			a2len;
+		int			r;
+
+		if (len1 >= TEXTBUFLEN / 2)
+		{
+			a1len = len1 * 2 + 2;
+			a1p = palloc(a1len);
+		}
+		else
+		{
+			a1len = TEXTBUFLEN;
+			a1p = a1buf;
+		}
+		if (len2 >= TEXTBUFLEN / 2)
+		{
+			a2len = len2 * 2 + 2;
+			a2p = palloc(a2len);
+		}
+		else
+		{
+			a2len = TEXTBUFLEN;
+			a2p = a2buf;
+		}
+
+		/* API does not work for zero-length input */
+		if (len1 == 0)
+			r = 0;
+		else
+		{
+			r = MultiByteToWideChar(CP_UTF8, 0, arg1, len1,
+									(LPWSTR) a1p, a1len / 2);
+			if (!r)
+				ereport(ERROR,
+						(errmsg("could not convert string to UTF-16: error code %lu",
+								GetLastError())));
+		}
+		((LPWSTR) a1p)[r] = 0;
+
+		if (len2 == 0)
+			r = 0;
+		else
+		{
+			r = MultiByteToWideChar(CP_UTF8, 0, arg2, len2,
+									(LPWSTR) a2p, a2len / 2);
+			if (!r)
+				ereport(ERROR,
+						(errmsg("could not convert string to UTF-16: error code %lu",
+								GetLastError())));
+		}
+		((LPWSTR) a2p)[r] = 0;
+
+		errno = 0;
+#ifdef HAVE_LOCALE_T
+		if (locale)
+			result = wcscoll_l((LPWSTR) a1p, (LPWSTR) a2p, locale->info.lt);
+		else
+#endif
+			result = wcscoll((LPWSTR) a1p, (LPWSTR) a2p);
+		if (result == 2147483647)	/* _NLSCMPERROR; missing from mingw
+									 * headers */
+			ereport(ERROR,
+					(errmsg("could not compare Unicode strings: %m")));
+
+		/* Break tie if necessary. */
+		if (result == 0 &&
+			(!locale || locale->deterministic))
+		{
+			result = memcmp(arg1, arg2, Min(len1, len2));
+			if ((result == 0) && (len1 != len2))
+				result = (len1 < len2) ? -1 : 1;
+		}
+
+		if (a1p != a1buf)
+			pfree(a1p);
+		if (a2p != a2buf)
+			pfree(a2p);
+
+		return result;
+	}
+#endif							/* WIN32 */
+
+	if (len1 >= TEXTBUFLEN)
+		a1p = (char *) palloc(len1 + 1);
+	else
+		a1p = a1buf;
+	if (len2 >= TEXTBUFLEN)
+		a2p = (char *) palloc(len2 + 1);
+	else
+		a2p = a2buf;
+
+	memcpy(a1p, arg1, len1);
+	a1p[len1] = '\0';
+	memcpy(a2p, arg2, len2);
+	a2p[len2] = '\0';
+
+	if (locale)
+	{
+		if (locale->provider == COLLPROVIDER_ICU)
+		{
+#ifdef USE_ICU
+#ifdef HAVE_UCOL_STRCOLLUTF8
+			if (GetDatabaseEncoding() == PG_UTF8)
+			{
+				UErrorCode	status;
+
+				status = U_ZERO_ERROR;
+				result = ucol_strcollUTF8(locale->info.icu.ucol,
+										  arg1, len1,
+										  arg2, len2,
+										  &status);
+				if (U_FAILURE(status))
+					ereport(ERROR,
+							(errmsg("collation failed: %s", u_errorName(status))));
+			}
+			else
+#endif
+			{
+				int32_t		ulen1,
+					ulen2;
+				UChar	   *uchar1,
+					*uchar2;
+
+				ulen1 = icu_to_uchar(&uchar1, arg1, len1);
+				ulen2 = icu_to_uchar(&uchar2, arg2, len2);
+
+				result = ucol_strcoll(locale->info.icu.ucol,
+									  uchar1, ulen1,
+									  uchar2, ulen2);
+
+				pfree(uchar1);
+				pfree(uchar2);
+			}
+#else							/* not USE_ICU */
+			/* shouldn't happen */
+			elog(ERROR, "unsupported collprovider: %c", locale->provider);
+#endif							/* not USE_ICU */
+		}
+		else
+		{
+#ifdef HAVE_LOCALE_T
+			result = strcoll_l(a1p, a2p, locale->info.lt);
+#else
+			/* shouldn't happen */
+			elog(ERROR, "unsupported collprovider: %c", locale->provider);
+#endif
+		}
+	}
+	else
+		result = strcoll(a1p, a2p);
+
+	/* Break tie if necessary. */
+	if (result == 0 &&
+		(!locale || locale->deterministic))
+		result = strcmp(a1p, a2p);
+
+	if (a1p != a1buf)
+		pfree(a1p);
+	if (a2p != a2buf)
+		pfree(a2p);
+
+	return result;
+}
+
+/*
+ * pg_strncoll
+ *
+ * Call ucol_strcollUTF8(), ucol_strcoll(), strcoll(), strcoll_l(), wcscoll(),
+ * or wcscoll_l() as appropriate for the given locale, platform, and database
+ * encoding. If the locale is not specified, use the database collation.
+ *
+ * Arguments must be encoded in the database encoding.
+ *
+ * If the collation is deterministic, break ties with memcmp(), and then with
+ * the string length.
+ */
+int
+pg_strncoll(const char *arg1, size_t len1, const char *arg2, size_t len2,
+			pg_locale_t locale)
+{
+	char	*arg1n = palloc(len1 + 1);
+	char	*arg2n = palloc(len2 + 1);
+	int		 result;
+
+	/* nul-terminate arguments and call pg_strcoll() */
+	memcpy(arg1n, arg1, len1);
+	arg1n[len1] = '\0';
+	memcpy(arg2n, arg2, len2);
+	arg2n[len2] = '\0';
+
+	result = pg_strcoll(arg1n, arg2n, locale);
+
+	pfree(arg1n);
+	pfree(arg2n);
+
+	return result;
+}
 
 #ifdef USE_ICU
 /*
