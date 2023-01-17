3b58b6da537da449eb82e76dcb50fbceb3ac6fd9
Jeff Davis <jeff@j-davis.com>
2023-01-16 16:37:55 -0800

    fixup control and SQL

--- contrib/icu_multilib/icu_multilib--1.0.sql
+++ contrib/icu_multilib/icu_multilib--1.0.sql
@@ -3,7 +3,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION icu_multilib" to load this file. \quit
 
-CREATE FUNCTION icu_multilib.icu_library_versions(
+CREATE FUNCTION icu_library_versions(
     icu_version OUT TEXT,
     unicode_version OUT TEXT,
     cldr_version OUT TEXT,
@@ -13,11 +13,11 @@ CREATE FUNCTION icu_multilib.icu_library_versions(
   RETURNS SETOF RECORD LANGUAGE C
   AS 'MODULE_PATHNAME', 'icu_library_versions';
 
-CREATE FUNCTION icu_multilib.icu_collation_versions(
+CREATE FUNCTION icu_collation_versions(
     locale IN TEXT,
     icu_version OUT TEXT,
     uca_version OUT TEXT,
-    collator_version OUT TEXT,
+    collator_version OUT TEXT
   )
   RETURNS SETOF RECORD LANGUAGE C
   AS 'MODULE_PATHNAME', 'icu_collation_versions';
