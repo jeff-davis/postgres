c5a2c2ce0f1dc3dc8d83f3d8d257c077b0fa4f96
Jeff Davis <jeff@j-davis.com>
2023-03-24 16:09:44 -0700

    initdb: emit message when using default ICU locale.
    Helpful to determine from test logs whether the locale came from the
    environment or a command-line option.
    
    Discussion: https://postgr.es/m/04182066-7655-344a-b8b7-040b1b2490fb%40enterprisedb.com

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -2340,7 +2340,10 @@ setlocales(void)
 #ifdef USE_ICU
 		/* acquire default locale from the environment, if not specified */
 		if (icu_locale == NULL)
+		{
 			icu_locale = default_icu_locale();
+			printf(_("Using default ICU locale \"%s\".\n"), icu_locale);
+		}
 
 		/*
 		 * In supported builds, the ICU locale ID will be checked by the
