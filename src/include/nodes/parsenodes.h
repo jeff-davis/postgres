3afc607764288b51c4385d22111f2de23ee630d9
Jeff Davis <jeff@j-davis.com>
2024-01-02 13:42:48 -0800

    CREATE SUBSCRIPTION ... SERVER.
    --- CATVERSION BUMP ---
    
    Allow specifying a foreign server for CREATE SUBSCRIPTION, rather than
    a raw connection string with CONNECTION.
    
    Using a foreign server as a layer of indirection improves management
    of multiple subscriptions to the same server. It also provides
    integration with user mappings in case different subscriptions have
    different owners or a subscription changes owners.
    
    Reviewed-by: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Reviewed-by: Shlok Kyal <shlok.kyal.oss@gmail.com>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Discussion: https://postgr.es/m/61831790a0a937038f78ce09f8dd4cef7de7456a.camel@j-davis.com

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -4380,6 +4380,7 @@ typedef struct CreateSubscriptionStmt
 {
 	NodeTag		type;
 	char	   *subname;		/* Name of the subscription */
+	char	   *servername;		/* Server name of publisher */
 	char	   *conninfo;		/* Connection string to publisher */
 	List	   *publication;	/* One or more publication to subscribe to */
 	List	   *options;		/* List of DefElem nodes */
@@ -4388,6 +4389,7 @@ typedef struct CreateSubscriptionStmt
 typedef enum AlterSubscriptionType
 {
 	ALTER_SUBSCRIPTION_OPTIONS,
+	ALTER_SUBSCRIPTION_SERVER,
 	ALTER_SUBSCRIPTION_CONNECTION,
 	ALTER_SUBSCRIPTION_SET_PUBLICATION,
 	ALTER_SUBSCRIPTION_ADD_PUBLICATION,
@@ -4403,6 +4405,7 @@ typedef struct AlterSubscriptionStmt
 	NodeTag		type;
 	AlterSubscriptionType kind; /* ALTER_SUBSCRIPTION_OPTIONS, etc */
 	char	   *subname;		/* Name of the subscription */
+	char	   *servername;		/* Server name of publisher */
 	char	   *conninfo;		/* Connection string to publisher */
 	List	   *publication;	/* One or more publication to subscribe to */
 	List	   *options;		/* List of DefElem nodes */
