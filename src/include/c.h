904a4b30319115c42fb4a696a59beee74f3e3daf
Jeff Davis <jeff@j-davis.com>
2025-10-21 13:16:47 -0700

    Use C11 char16_t and char32_t for Unicode code points.
    Reviewed-by: Tatsuo Ishii <ishii@postgresql.org>
    Reviewed-by: Thomas Munro <thomas.munro@gmail.com>
    Reviewed-by: Peter Eisentraut <peter@eisentraut.org>
    Discussion: https://postgr.es/m/bedcc93d06203dfd89815b10f815ca2de8626e85.camel%40j-davis.com

--- src/include/c.h
+++ src/include/c.h
@@ -518,6 +518,26 @@ typedef void (*pg_funcptr_t) (void);
 #include <stdbool.h>
 
 
+/*
+ * char16_t and char32_t
+ *      Unicode code points.
+ *
+ * uchar.h should always be available in C11, but it's not available on
+ * Mac. However, these types are keywords in C++11, so when using C++, we
+ * can't redefine the types.
+ *
+ * XXX: when uchar.h is available everywhere, we can remove this check and
+ * just include uchar.h unconditionally.
+ *
+ * XXX: this section is out of place because uchar.h needs to be included
+ * after port.h, due to an interaction with win32_port.h in some cases.
+ */
+#ifndef __cplusplus
+typedef uint16_t char16_t;
+typedef uint32_t char32_t;
+#endif
+
+
 /* ----------------------------------------------------------------
  *				Section 3:	standard system types
  * ----------------------------------------------------------------
