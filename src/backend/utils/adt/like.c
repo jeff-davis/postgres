a9f365b0ebd0c71ad2fec3bba8dbf7a21b502e3a
Jeff Davis <jeff@j-davis.com>
2025-10-26 14:59:40 -0700

    Avoid global LC_CTYPE dependency in like.c.
    Call char_tolower() instead of pg_tolower().

--- src/backend/utils/adt/like.c
+++ src/backend/utils/adt/like.c
@@ -96,7 +96,14 @@ SB_lower_char(unsigned char c, pg_locale_t locale)
 	if (locale->ctype_is_c)
 		return pg_ascii_tolower(c);
 	else if (locale->is_default)
-		return pg_tolower(c);
+	{
+		if (c >= 'A' && c <= 'Z')
+			return c + ('a' - 'A');
+		else if (IS_HIGHBIT_SET(c))
+			return char_tolower(c, locale);
+		else
+			return c;
+	}
 	else
 		return char_tolower(c, locale);
 }
